5a34aef104c3164968fb94e0242f2ce8da481bbe
Sevinch Xayriddinova <xayiddinovasevinch@gmail.com>
2023-10-04 20:02:54 +0500

    Добавьте файлы проекта.

---
+++ 27-savol/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+using (HttpClient client = new HttpClient())
+{
+    string bUrl = "https://jsonplaceholder.typicode.com/posts";
+    HttpResponseMessage message = await client.GetAsync(bUrl);
+
+    string responce = await message.Content.ReadAsStringAsync();
+
+  
+    var users = JsonConvert.DeserializeObject<List<User>>(responce);
+    
+    users.ForEach(x=>Console.WriteLine(x.title));
+    
+
+}
+
+class User
+{
+    public int userId { get; set; }
+    public int id { get; set; }
+    public string title { get; set; }
+    public string body { get; set; }
+}
